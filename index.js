e66a4e3a5595dc496464d4e61edb0fb4cab62704
Nick Savva <nsavva38@gmail.com>
2025-04-11 21:02:28 -0400

    added eventlistener for keydown for movement and projectiles

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ const animate = () => {
 }
 
 animate();
+
+// can pass 'event' into the param instead of ({key})
+// but the ({key}) way allows us to destructure
+addEventListener('keydown', ({key}) => {
+  switch (key) {
+    case 'a':
+      console.log('left');
+      break;
+    case 'd':
+      console.log('right');
+      break;
+    case 'w':
+      console.log('up');
+      break;
+    case 's':
+      console.log('down');
+      break;
+    case ' ':
+      console.log('spacebar');
+      break;
+  }
+})
